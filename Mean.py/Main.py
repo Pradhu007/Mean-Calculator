ab5b187549255b4d4ca7d4bb92507d8b3cae5948
Pradhu007 <55123592+Pradhu007@users.noreply.github.com>
2022-08-12 19:40:49 +1000

    Update Main.py

--- Mean.py/Main.py
+++ Mean.py/Main.py
@@ -1,17 +1,21 @@
-sum = 0
-total = 0
-b = 1
+sum = 0 #Keeps updating the values of the numbers entered
+start = 0 #Indicates the start of the loop
+end = 5 #You can change this value depending on the amount of numbers you want to calculate to get the mean.
+#For example, say if you want to find the mean of 5 numbers(by default), you change the end variable to hold the value 5 .  
 count = 0
 done = False
-while b < 5 and done!= True:
+while start < end and done!= True:
     number = int(input("Enter number {}".format(b)))
-    sum = sum  + number
-    count = count + 1
+    sum = sum  + number #Adds numbers to the sum variable as numbers are inputted 
+    count = count + 1#Keeps track of the count of numbers that were inputted
 
-    b = b + 1
+    start = start + 1 #Increments the control variable "start"
 
-    if b == 12:
+    if start == end:
         done = True
-        break
+        break #This statement which terminates the loop. 
 
-print("The mean of these numbers is {}".format(sum/count))
+total = sum/count
+print("The mean of these numbers is {}".format(total))
+
+#Credits:Pradhu007
